0175b68616a107c6388e9ba4e90cb80e24f20208
Vuong Do <xoaic@xcc.one>
2023-10-25 00:22:01 +0700

    UPDATE 24/10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Homepage() {
     return (
         <div className="w-screen h-screen overflow-x-hidden">
             <Background />
-            <div className="w-full relative z-1">    
+            <div className="w-full">    
                 <header>
                     <Navbar />
                 </header>
@@ -17,7 +17,7 @@ export default function Homepage() {
                 </main>
 
                 <footer>
-                    <p className="specialFont text-center text-2xl text-white/50 hover:text-white/100 hover:bg-orange-300/30">Copyright by XoaiCC.</p>
+                    <p className="specialFont text-center text-2xl text-white/50 hover:text-white/100 hover:bg-orange-300/30 cursor-default">Copyright by XoaiCC.</p>
                 </footer>
             </div>
         </div>
